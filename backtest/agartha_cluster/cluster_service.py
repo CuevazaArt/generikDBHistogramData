5816c65a2a0b3810416c5478705c030204ce5a36
CuevazaArt <artificialconsciousnessmachine@protonmail.com>
2026-06-14 15:33:21 -0600

    chore: bump cluster version to 0.1.5

--- backtest/agartha_cluster/cluster_service.py
+++ backtest/agartha_cluster/cluster_service.py
@@ -34,7 +34,7 @@ from backtest.agartha_cluster.models import (
 from backtest.agartha_cluster.reconciler import Reconciler
 from backtest.agartha_cluster.scheduler import DeployScheduler
 
-CLUSTER_VERSION = "0.1.4"
+CLUSTER_VERSION = "0.1.5"
 
 
 @dataclass
